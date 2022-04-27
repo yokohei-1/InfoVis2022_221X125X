e20b458b112018d2e0af26a8a095f10b86ccfd84
yokohei-1 <60385247+yokohei-1@users.noreply.github.com>
2022-04-27 14:29:01 +0900

    タスク1_テスト

--- W06/w06_task1_main.js
+++ W06/w06_task1_main.js
@@ -37,7 +37,7 @@ function ShowScatterPlot(data) {
         .call(xaxis);
 
     svg.append('g')
-        .attr('transform', `translate(${width - margin.left - margin.right}, 0)`)
+        .attr('transform', `translate(${- margin.left}, 0)`)
         .call(yaxis);
 
     svg.selectAll("circle")
