d1be9a5b0894515e5db18a6bd047b547036bba65
yokohei-1 <60385247+yokohei-1@users.noreply.github.com>
2022-05-11 14:46:21 +0900

    修正

---
+++ W08/w08_task02_main.js
@@ -0,0 +1,101 @@
+d3.csv("https://yokohei-1.github.io/InfoVis2022_221X125X/W08/task1_data.csv")
+    .then(data => {
+        data.forEach(d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 128,
+            //margin: { top: 10, right: 10, bottom: 20, left: 60 }
+        };
+
+        const scatter_plot = new ScatterPlot(config, data);
+        scatter_plot.update();
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+class ScatterPlot {
+
+    constructor(config, data) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 128,
+            //margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 }
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select(self.config.parent)
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.line = d3.line()
+            .x(d => d.x)
+            .y(d => d.y);
+
+        /*self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);*/
+
+        //self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        //self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        /*self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
+
+        self.yscale = d3.scaleBand()
+            .domain(self.data.map(d => d.label))
+            .range([0, self.inner_height]);
+
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(5);
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis = d3.axisLeft(self.yscale)
+            .tickSizeOuter(0);
+
+        self.yaxis_group = self.chart.append('g');*/
+        //.attr('transform', `translate(10, 10)`);
+    }
+
+    update() {
+        let self = this;
+
+        /*const xmin = d3.min(self.data, d => d.value);
+        const xmax = d3.max(self.data, d => d.value);
+        self.xscale.domain([0, xmax]);
+
+        self.xaxis.tickSizeOuter(0);
+
+        self.yscale.paddingInner(0.1);*/
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        /*self.xaxis_group
+            .call(self.xaxis);
+
+        self.yaxis_group
+            .call(self.yaxis);*/
+
+        self.chart.selectAll("path")
+            .data(self.data)
+            .enter()
+            .append("path")
+            .attr('d', line(data))
+            .attr('stroke', 'black')
+            .attr('fill', 'none');
+
+    }
+}
